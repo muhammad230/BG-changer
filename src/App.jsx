d13fd906858d00cf5cee27c05a6df88a2383a74b
muhammad230 <ali.khansmr3@gmail.com>
2026-04-29 16:39:04 +0500

    add new colors

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,16 @@ function App() {
     >
       <div className='flex flex-wrap fixed justify-center bottom-12 inset-x-0 px-2 '> 
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-             <button onClick={()=>{
-              setcolor("red")
-             }} className='outline-none px-4 py-1 rounded-full text-white'
+             <button onClick={()=>{setcolor("red")}} className='outline-none px-4 py-1 rounded-full text-white'
              style={{backgroundColor: "red"}}
              >red </button>
-             <button className='outline-none px-4 rounded-full'
+             <button  onClick={()=>{setcolor("blue")}} className='outline-none px-4 rounded-full'
              style={{backgroundColor: "blue"}}
              >blue </button>
-             <button className='outline-none px-4 rounded-full'
+             <button  onClick={()=>{setcolor("yellow")}} className='outline-none px-4 rounded-full'
              style={{backgroundColor: "yellow"}}
              >yellow </button>
-             <button className='outline-none px-4 rounded-full'
+             <button  onClick={()=>{setcolor("pink")}}  className='outline-none px-4 rounded-full'
              style={{backgroundColor: "pink"}}
              >pink </button>
         </div>
